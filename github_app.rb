cfb7af5fa4070df7ff9a4807eb627ebb0d206227
Tatsumi0000 <datsunote0211@gmail.com>
2022-12-11 06:26:16 +0900

    クラスに書いた

--- github_app.rb
+++ github_app.rb
@@ -1,9 +1,17 @@
 require 'jwt'
 require 'octokit'
 
-# JWT TOKENを生成する
-def create_jwt_token
-    # Private key contents
+class GitHubApiRequest
+
+  def initialize(repository_name)
+    @client = Octokit::Client.new
+    @repository_name = repository_name
+    create_jwt_token
+  end
+
+  # JWT TOKENを生成する
+  def create_jwt_token
+    # ダウンロードした秘密鍵
     private_pem = File.read("./XXX.pem")
     private_key = OpenSSL::PKey::RSA.new(private_pem)
       
@@ -13,17 +21,37 @@ def create_jwt_token
       iat: Time.now.to_i - 60,
       # JWT expiration time (10 minute maximum)
       exp: Time.now.to_i + (10 * 60),
-      # GitHub App's identifier
+      # 今回作成したGitHub AppのID
       # https://git.pepabo.com/organizations/colorme/settings/apps/colorme-token-app
       iss: "YYY"
     }
       
     jwt = JWT.encode(payload, private_key, "RS256")
-    return jwt
-end
+    @client.bearer_token = jwt
+  end
 
-# 
-def reuquest_github_app_info
-    client = Octokit::Client.new(bearer_token: create_jwt_token)
-    p client.app
+  private :create_jwt_token
+
+  # installation_idを取得
+  def get_installation_id_from_github_api
+    installation_id = @client.find_repository_installation(@repository_name).id
+    return installation_id
+  end
+
+  # アクセストークンを生成
+  def create_access_token(installation_id)
+    access_token = @client.create_app_installation_access_token(installation_id).token
+    return access_token
+  end
+
+  # アクセストークンを生成
+  def set_access_token(access_token)
+    @client.bearer_token = access_token
+  end
+
+  # GitHub Releasesを作成
+  def create_github_release(tag_name)
+    @client.create_release(@repository_name, tag_name)
+  end
+
 end
