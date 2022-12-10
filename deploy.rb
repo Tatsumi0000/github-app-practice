7bbd049ac2436532fdf9284cc282182a93e7f528
Tatsumi0000 <datsunote0211@gmail.com>
2022-12-11 03:16:16 +0900

    tag_nameはRequiredで必須

--- deploy.rb
+++ deploy.rb
@@ -2,7 +2,7 @@ require 'octokit'
 
 def deploy_to_github_releases
     client = Octokit::Client.new(bearer_token: ENV['TOKEN'])
-    client.create_release(ENV['GITHUB_REPOSITORY'], ENV['SHA'])
+    client.create_release(ENV['GITHUB_REPOSITORY'], tag_name: ENV['SHA'])
 end
 
 if __FILE__ == $0
