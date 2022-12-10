a802e46e48b772fb7ad04136e9a843109adab95e
Tatsumi0000 <datsunote0211@gmail.com>
2022-12-11 06:26:33 +0900

    動かした

--- main.rb
+++ main.rb
@@ -1,5 +1,9 @@
 require_relative 'github_app'
 
 if __FILE__ == $0
-    reuquest_github_app_info
+    github_app = GitHubApp.new('Tatsumi0000/github-app-practice')
+    installation_id = github_app.get_installation_id_from_github_api
+    access_token = github_app.create_access_token(installation_id)
+    github_app.set_access_token(access_token)
+    github_app.create_github_release("v3.0.0")
 end
